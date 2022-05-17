7e3e4ec40c0138a32784f3ba7d09e3a864009bef
HennCH <2132132@champlaincollege.qc.ca>
2022-05-17 19:51:03 -0400

    Initial commit

---
+++ src/main/java/com/example/fun4kidz/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.fun4kidz;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
