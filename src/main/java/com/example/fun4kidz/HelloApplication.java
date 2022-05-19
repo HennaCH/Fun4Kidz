57bcc88658be7e5ec48849ed29010c5138fb70e9
HennCH <2132132@champlaincollege.qc.ca>
2022-05-18 21:08:25 -0400

    Add shape to scene

--- src/main/java/com/example/fun4kidz/HelloApplication.java
+++ src/main/java/com/example/fun4kidz/HelloApplication.java
@@ -1,12 +1,19 @@
 package com.example.fun4kidz;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,17 +30,31 @@ public class HelloApplication extends Application {
         primaryStage.centerOnScreen();
 
         Group grp = new Group();
+        HBox hbox = new HBox();
         Scene scene = new Scene(grp);
+
         Button button1 = new Button("Car");
         button1.setLayoutX(10);
         button1.setLayoutY(120);
+
+
         Button button2 = new Button("Flower");
         button2.setLayoutX(10);
         button2.setLayoutY(160);
+
         Button button3 = new Button("Butterfly");
         button3.setLayoutX(10);
         button3.setLayoutY(200);
-        grp.getChildren().addAll(button1,button2,button3);
+
+        Rectangle rect = new Rectangle();
+        rect.setFill(Color.PINK);
+        rect.setWidth(300);
+        rect.setHeight(200);
+        rect.setLayoutX(290);
+        rect.setLayoutY(150);
+
+
+        grp.getChildren().addAll(button1, button2, button3,rect);
         primaryStage.setScene(scene);
         primaryStage.show();
 
---
+++ src/main/java/com/example/fun4kidz/Show.java
@@ -0,0 +1,11 @@
+package com.example.fun4kidz;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public interface Show {
+
+
+
+}
